a81986cc620b65832853afd4f8de5ccc230877a9
harverma <85237897+harverma@users.noreply.github.com>
2021-06-22 21:50:22 +0530

    variable

---
+++ variable.tf
@@ -0,0 +1,68 @@
+#====================#
+# vCenter connection #
+#====================#
+
+variable "vsphere_user" {
+  description = "vSphere user name"
+   sensitive = true
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  sensitive = true
+}
+
+variable "vsphere_server" {
+  description = "vCenter server FQDN or IP"
+}
+
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster"
+  default     = ""
+}
+
+#=========================#
+# vSphere virtual machine #
+#=========================#
+
+variable "vm_datastore" {
+  description = "Datastore used for the vSphere virtual machines"
+}
+
+variable "vm_network" {
+  description = "Network used for the vSphere virtual machines"
+}
+
+variable "vm_template" {
+  description = "Template used to create the vSphere virtual machines"
+}
+
+variable "vm_linked_clone" {
+  description = "Use linked clone to create the vSphere virtual machine from the template (true/false). If you would like to use the linked clone feature, your template need to have one and only one snapshot"
+  default = "false"
+}
+
+variable "vm_ip" {
+  description = "Ip used for the vSpgere virtual machine"
+}
+
+variable "vm_domain" {
+  description = "Domain for the vSphere virtual machine"
+}
+
+variable "vm_cpu" {
+  description = "Number of vCPU for the vSphere virtual machines"
+}
+
+variable "vm_ram" {
+  description = "Amount of RAM for the vSphere virtual machines (example: 2048)"
+}
+
+variable "vm_name" {
+  description = "The name of the vSphere virtual machines and the hostname of the machine"
+}
