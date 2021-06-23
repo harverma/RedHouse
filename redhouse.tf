771cf89607a17a216da400338f973e7802e17114
harverma <85237897+harverma@users.noreply.github.com>
2021-06-23 12:16:04 +0530

    add ip

--- redhouse.tf
+++ redhouse.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
   user           = "${var.vsphere_user}"
   password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_vcenter}"
+  vsphere_server = "10.31.50.52"
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
