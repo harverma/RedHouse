a45c6d5f05d1d3ac668a8dfb5e3a5035892f002c
harverma <85237897+harverma@users.noreply.github.com>
2021-06-23 18:08:05 +0530

    Create simpsons.tf

---
+++ simpsons.tf
@@ -0,0 +1,121 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@harverma 
+harverma
+/
+VmWare
+1
+0
+0
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+VmWare/redhouse.tf
+@harverma
+harverma name
+Latest commit 19c4831 19 minutes ago
+ History
+ 1 contributor
+76 lines (62 sloc)  2.04 KB
+  
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "10.31.50.52"
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "${var.vsphere_datacenter}"
+}
+
+data "vsphere_datacenter" "temp" {
+  name = "${var.vsphere_datacenter_temp}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore-46"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+  data "vsphere_resource_pool" "pool" {
+  name          = "Eng_Prod_Pool"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "Ansible"
+  datacenter_id = "${data.vsphere_datacenter.temp.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "Ansible-Hv1"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  num_cpus = "${var.vm_cpu}"
+  memory   = "${var.vm_ram}"
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    
+      
+
+    customize {
+      timeout = 50
+      linux_options {
+        host_name = "Ansible-Hv"
+        domain    = "ansible.simpsons.qa"
+      }
+      network_interface {}
+    }
+  }
+}
+
+output "my_ip_address" {
+ value = "${vsphere_virtual_machine.vm.default_ip_address}"
+}
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
