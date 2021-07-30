2ace08048f09ac8c678b2888085905b993fcf0d8
harverma <85237897+harverma@users.noreply.github.com>
2021-07-30 15:35:28 +0530

    remove

--- simpsons.tf
+++ simpsons.tf
@@ -71,14 +71,7 @@ resource "vsphere_virtual_machine" "vm" {
       windows_options {
         computer_name = "${var.vm_name}"
         admin_password          = "control*88"
-        auto_logon              = true
-        auto_logon_count        = 1
-        run_once_command_list = [
-           "cmd.exe /C Powershell.exe -ExecutionPolicy -command 'get-volume' ",
-           "cmd.exe /C Powershell.exe -ExecutionPolicy -command '$size = Get-PartitionSupportedSize -DriveLetter C' ",
-           "cmd.exe /C Powershell.exe -ExecutionPolicy -command 'Resize-Partition -DriveLetter C -Size $size.SizeMax' ",
-        ]   
-        
+                
       }
       network_interface {}
     }
