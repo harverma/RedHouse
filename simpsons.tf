32b842c833b4710581d37ecf76d98d9f28961825
harverma <85237897+harverma@users.noreply.github.com>
2021-10-07 11:55:04 +0530

    Update simpsons.tf

--- simpsons.tf
+++ simpsons.tf
@@ -40,50 +40,50 @@ data "vsphere_virtual_machine" "template" {
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "${var.vm_name}"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  num_cpus = "${var.vm_cpu}"
-  memory   = "${var.vm_ram}"
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
-  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+    name             = "${var.vm_name}"
+    resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+    datastore_id     = "${data.vsphere_datastore.datastore.id}"
+    num_cpus = "${var.vm_cpu}"
+    memory   = "${var.vm_ram}"
+    guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+    scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
 
- network_interface {
-    network_id   = "${data.vsphere_network.network.id}"
-    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
-  }
+    network_interface {
+      network_id   = "${data.vsphere_network.network.id}"
+      adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+    }
 
-  disk {
-    label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
-  }
+    disk {
+      label            = "disk0"
+      size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+      eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+      thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    }
    
-  disk {
-    label            = "disk1"
-    size             = "${data.vsphere_virtual_machine.template.disks.1.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.1.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.1.thin_provisioned}"
-    unit_number = 1
-  } 
+   disk {
+     label            = "disk1"
+     size             = "${data.vsphere_virtual_machine.template.disks.1.size}"
+     eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.1.eagerly_scrub}"
+     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.1.thin_provisioned}"
+     unit_number = 1
+    } 
 
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
     timeout = 180
       customize {
-      timeout =30
-         windows_options {
-           computer_name = "${var.vm_name}"
-           admin_password = "${var.local_adminpass}"
-           join_domain      = "${var.windomain}"
-           domain_admin_user = "${var.domain_admin_user}"
-           domain_admin_password = "${var.domain_admin_password}" 
+        timeout =30
+        windows_options {
+             computer_name = "${var.vm_name}"
+             admin_password = "${var.local_adminpass}"
+             join_domain      = "${var.windomain}"
+             domain_admin_user = "${var.domain_admin_user}"
+             domain_admin_password = "${var.domain_admin_password}" 
          }
-      network_interface {}
+        network_interface {}
+      }
     }
   }
-}
 
    output "my_ip_address" {
    value = "${vsphere_virtual_machine.vm.default_ip_address}"
