a153663d8d70cf91b4dc0c6bb7604ac09133ec50
harverma <85237897+harverma@users.noreply.github.com>
2021-11-16 20:46:46 +0530

    Merge branch 'rnd' of https://github.com/harverma/VmWare into rnd

---
+++ sqlsetup/sqlconfig.ps1
@@ -0,0 +1,4 @@
+robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SQL Server 2019\Enterprise Edition'  'C:\MSSQL2019\'
+robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
+C:\MSSQL2019\SW_DVD9_SQL_Svr_Enterprise_Edtn_2019Dec2019_64Bit_English_MLF_X22-22247.ISO
+sllep -s 5
